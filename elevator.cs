cc9a4fb1ae7c4f2e4cb9a580303a73c6de86cbf7
DragonZX <DragonZX@yandex.ru>
2022-07-01 15:30:07 +0300

    Using ini for set up

--- elevator.cs
+++ elevator.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
-
+using Ini;
 namespace elevator
 {
     internal static class Start
@@ -8,18 +8,29 @@ namespace elevator
         private static void Main(string[] args)
         {
             var ep = new Process();
-            const string exec = "autorun-old.exe";
+            string exec = "autorun-old.exe";
+            string IniFile = "elevator.ini";
+            string fargs = "";
+            string wnferror = exec + " was not found";
+            if (File.Exists(IniFile))
+            {
+                var MyIni = new IniFile(IniFile);
+                exec = MyIni.Read("openfile", "elevator");
+                fargs = MyIni.Read("params", "elevator");
+                wnferror = MyIni.Read("notfound","elevator");
+                // var wdir = MyIni.Read("workdir","elevator");
+            }
             if (File.Exists(exec))
             {
                 ep.StartInfo.FileName = exec;
-                ep.StartInfo.Arguments = "";
+                ep.StartInfo.Arguments = fargs;
                 ep.StartInfo.CreateNoWindow = true;
                 ep.StartInfo.UseShellExecute = false;
                 ep.Start();
             }
             else
             {
-                System.Console.WriteLine(exec + " was not found"); //error message
+                System.Console.WriteLine(wnferror); //error message
                 System.Threading.Thread.Sleep(1500); //Pause before closing
             }
         }
