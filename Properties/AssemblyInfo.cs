4c6f2627adaaeb1189850070e7a70351c30c451b
DragonZX <DragonZX@yandex.ru>
2022-07-01 14:25:35 +0300

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Elevator")]
+[assembly: AssemblyDescription("Used to elevate the startup object with Admin rights")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Elevator.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9fc3b614-938a-4113-ae4f-aa951a8527bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
---
+++ elevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace elevator
+{
+    internal static class Start
+    {
+        private static void Main(string[] args)
+        {
+            var ep = new Process();
+            const string exec = "autorun-old.exe";
+            if (File.Exists(exec))
+            {
+                ep.StartInfo.FileName = exec;
+                ep.StartInfo.Arguments = "";
+                ep.StartInfo.CreateNoWindow = true;
+                ep.StartInfo.UseShellExecute = false;
+                ep.Start();
+            }
+            else
+            {
+                System.Console.WriteLine(exec + " was not found"); //error message
+                System.Threading.Thread.Sleep(1500); //Pause before closing
+            }
+        }
+    }
+}
